919f49e39ce5871e88967940156b3b6d14f6936c
Kevin Laminto <kevin.laminto@gmail.com>
2020-07-31 13:40:52 +1000

    Feature: Filter

--- Ghibliii/Ghibliii/App/Home/HomeViewController.swift
+++ Ghibliii/Ghibliii/App/Home/HomeViewController.swift
@@ -52,6 +52,12 @@ class HomeViewController: UICollectionViewController {
         searchController.searchResultsUpdater = self
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
+        
+        // Setup bar button item
+        let filterButton =  UIButton(type: .custom)
+        filterButton.setImage(UIImage(systemName: "line.horizontal.3.decrease.circle"), for: .normal)
+        filterButton.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: filterButton)
     }
     
     /// Fetch the initial movies data
@@ -59,12 +65,12 @@ class HomeViewController: UICollectionViewController {
         if dataPersistEngine.films.isEmpty {
             API.shared.getData(type: Film.self, fromEndpoint: .film()) { [weak self] (films) in
                 guard let films = films else { return }
-                self?.films = films
+                self?.films = films.sorted(by: { $0.title < $1.title })
                 self?.dataPersistEngine.saveFilms(films)
                 self?.createSnapshot(from: films)
             }
         } else {
-            self.films = dataPersistEngine.films
+            self.films = dataPersistEngine.films.sorted(by: { $0.title < $1.title })
             self.createSnapshot(from: films)
         }
     }
@@ -80,6 +86,29 @@ class HomeViewController: UICollectionViewController {
         }
         self.present(navController, animated: true, completion: nil)
         
+    }
+    
+    @objc private func filterButtonTapped() {
+        let filterAlert = UIAlertController(title: "Filter by:", message: nil, preferredStyle: .actionSheet)
+        let titleFilterAction = UIAlertAction(title: "Name", style: .default) { (_) in
+            var snapshot = Snapshot()
+            snapshot.appendSections([.main])
+            snapshot.appendItems(self.films.sorted(by: { $0.title < $1.title }))
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+        let yearFilterAction = UIAlertAction(title: "Year", style: .default) { (_) in
+            var snapshot = Snapshot()
+            snapshot.appendSections([.main])
+            snapshot.appendItems(self.films.sorted(by: { $0.releaseDate < $1.releaseDate }))
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        filterAlert.addAction(titleFilterAction)
+        filterAlert.addAction(yearFilterAction)
+        filterAlert.addAction(cancelAction)
+        
+        self.present(filterAlert, animated: true, completion: nil)
     }
     
 }
@@ -98,10 +127,10 @@ extension HomeViewController {
             let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
             item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
             
-            let groupFractionalHeight: Float = isPhone ? 0.6 : 0.42
+            let groupFractionalHeight: CGFloat = isPhone ? 0.65 : 0.42
             let groupSize = NSCollectionLayoutSize(
                 widthDimension: .fractionalWidth(1),
-                heightDimension: .fractionalWidth(CGFloat(groupFractionalHeight)))
+                heightDimension: .fractionalWidth(groupFractionalHeight))
             let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: itemCount)
             group.edgeSpacing = .init(leading: .fixed(0), top: .fixed(10), trailing: .fixed(0), bottom: .fixed(10))
             let section = NSCollectionLayoutSection(group: group)
@@ -140,19 +169,6 @@ extension HomeViewController {
         snapshot.appendSections([.main])
         snapshot.appendItems(films)
         dataSource.apply(snapshot, animatingDifferences: true)
-    }
-    
-    fileprivate func adjustAnchorPointForGestureRecognizer(gestureRecognizer: UIGestureRecognizer) {
-        if gestureRecognizer.state == .began {
-            if let shapeViewToUse = gestureRecognizer.view {
-                let locationInView = gestureRecognizer.location(in: shapeViewToUse)
-                let locationInSuperview = gestureRecognizer.location(in: shapeViewToUse.superview)
-                shapeViewToUse.layer.anchorPoint =
-                    CGPoint(x: locationInView.x / shapeViewToUse.bounds.size.width,
-                            y: locationInView.y / shapeViewToUse.bounds.size.height)
-                shapeViewToUse.center = locationInSuperview
-            }
-        }
     }
     
     @objc fileprivate func hoverEffect(_ gestureRecognizer: UIHoverGestureRecognizer) {
